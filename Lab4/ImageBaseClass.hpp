3ef6a635f71669cb352a527175ec81cd0daf0ead
Fires-Mans <brian_w@hotmail.ca>
2013-11-14 11:25:33 -0500

    My base class for Images.

---
+++ Lab4/ImageBaseClass.hpp
@@ -0,0 +1,27 @@
+/*
+ * ImageBaseClass.hpp
+ *
+ *  Created on: Nov 12, 2013
+ *      Author: Owner
+ */
+
+#ifndef IMAGEBASECLASS_HPP_
+#define IMAGEBASECLASS_HPP_
+
+#include <iostream>
+using namespace std;
+
+class ImageBase{
+
+	private:
+
+
+};
+
+
+
+
+
+
+
+#endif /* IMAGEBASECLASS_HPP_ */
