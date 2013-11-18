37ef9813484a98dffd9df46c67471fa673f7116b
Fires-Mans <brian_w@hotmail.ca>
2013-11-18 15:54:53 -0500

    TEsta

---
+++ Lab4/MRI.hpp
@@ -0,0 +1,26 @@
+/*
+ * MRI.hpp
+ *
+ *  Created on: Nov 18, 2013
+ *      Author: Owner
+ */
+
+#ifndef MRI_HPP_
+#define MRI_HPP_
+
+class MRI: public ImageBase{
+
+	private:
+
+
+
+
+
+
+};
+
+
+
+
+
+#endif /* MRI_HPP_ */
