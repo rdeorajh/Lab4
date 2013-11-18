9df1432c4da69924dc5169900e9102a94b5a4874
AhmedNiri <AhmedNiri@10.16.108.22>
2013-11-18 13:14:14 -0500

    test

---
+++ lab4main/src/lab4main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : lab4main.cpp
+// Author      : Ahmed Niri
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
