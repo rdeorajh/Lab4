30410222f4d2c1bc5406b208b2f379e77793b215
Fires-Mans <brian_w@hotmail.ca>
2013-11-28 09:25:59 -0500

    My MRI derived class.

--- Lab4/MRI.hpp
+++ Lab4/MRI.hpp
@@ -8,13 +8,22 @@
 #ifndef MRI_HPP_
 #define MRI_HPP_
 
+#include <iostream>
+#include "ImageBaseClass.hpp"
+using namespace std;
+
 class MRI: public ImageBase{
 
 	private:
 
+	double magneticFieldStrength;   //magnetic field strength of mri machine
+	bool angioFlag;                //did they use angio? 1 is yes, 0 is no
+	string imagedNucleus;        //what nucleus was used to image? usually 1H?
 
-
-
+	public:
+	MRI();      //default constructor;
+	MRI(string FIle, int Bits, double mField, bool angio, string Nucleus); //constructor
+	void results();    //append image information and display image
 
 
 };
